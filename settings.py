694022f8c7f7e8b47f7d69d657564928e23b4a19
Alex <adundarov@gmail.com>
2023-11-07 14:36:09 -0800

    poo

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #Settings for the bot.
 TOKEN = 'MTE3MTIyNTgzMTk3NzI1NTAwMw.GGzDn9.kxHngmaERlBHMtAM8f1QB40ftMva0-ZfBGDiec'
-BotStatus = "with kids"
+BotStatus = "with kids ( [ )"
 Prefix = "["
 TEST_ID =1171554188749176893
-CHANNEL_ID = 604884555765121029
+CHANNEL_ID = 1171565775182643241
